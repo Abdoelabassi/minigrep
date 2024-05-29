e2e2fb7d082073ff182ea78449264a745065aff8
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-05-29 19:01:37 +0100

    handle reading a file

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,24 @@
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
+// author: EL Abassi Abderrazaq
+
+extern crate minigrep;
+use std::{env, process};
+
+use minigrep::Config;
 
 fn main() {
 
-    //let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-    // read file
-    let mut file = File::open("poem.txt").expect("File not found");
-    let mut content = String::new();
-
-    file.read_to_string(&mut content).expect("Something went wrong");
-
-    println!("file with test\n{}", content);
-
-
-}
-
-struct Config {
-    query: String,
-    filename: String
-}
-
-impl Config {
-    fn new(&args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
+    // parse arguments
+    let config = Config::new(&args).unwrap_or_else(|err|{
+        println!("Cannot parse arguments: {}", err);
+        process::exit(1);
+    });
+    //  read file
+    if let Err(e) = minigrep::run(config){
+        println!("Application error: {}", e);
+        process::exit(1);
     }
+
+
 }
